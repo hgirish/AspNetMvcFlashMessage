210ec932f8c15e56de78c92f5da5a723f5d4a5c5
Girish <girish@post.com>
2012-06-13 18:21:43 -0700

    Added ASP.NET MVC 4 project

---
+++ FlashMessageRailStyle/FlashMessageRailStyle/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FlashMessageRailStyle.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+          
+            return View();
+        }
+
+        public ActionResult Info()
+        {
+          TempData["info"] = "INFO flash message";
+          return RedirectToAction("Index");
+        }
+        public ActionResult Warning()
+        {
+        // warning will be displayed as warning has a priority over info
+          TempData["info"] = "INFO flash message";
+          TempData["warning"] = "WARNING flash message";
+          return RedirectToAction("Index");
+        }
+        public ActionResult Error()
+        {
+        // error will be displayed as error has a highest priority
+          TempData["info"] = "INFO flash message";
+          TempData["warning"] = "WARNING flash message";
+          TempData["error"] = "ERROR flash message";
+          return RedirectToAction("Index");
+        }
+        
+
+    }
+}
---
+++ FlashMessageRailStyle/FlashMessageRailStyle/Helpers/FlashHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace FlashMessageRailStyle.Helpers
+{
+  public static class FlashHelpers
+  {
+
+    public static void FlashInfo(this Controller controller, string message)
+    {
+      controller.TempData["info"] = message;
+    }
+    public static void FlashWarning(this Controller controller, string message)
+    {
+      controller.TempData["warning"] = message;
+    }
+    public static void FlashError(this Controller controller, string message)
+    {
+      controller.TempData["error"] = message;
+    }
+  }
+}
